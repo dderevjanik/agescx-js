0fd11ad468fc0b3a85f71f803cc693ac680bcb16
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2015-08-18 00:30:06 +0200

    extended docstring for ASDataView

--- modules/scenario-reader/ASDataView.ts
+++ modules/scenario-reader/ASDataView.ts
@@ -25,18 +25,26 @@ class ASDataView {
 		this._data = dataView;
 	}
 	
+	/**
+	 * skip number of bytes from current offset
+	 * @param bytes number of bytes to skip
+	 * @return offest position after skiping
+	 */
 	public skip(bytes: number): number{
 		this.offset += bytes;
 		return this.offset;
 	}
 	
+	/**
+	 * will jump to offset from starting point
+	 * @param offset to jump
+	 */
 	public toOffset(offset: number){
 		this.offset = offset;
 	}
 	
 	/**
 	 * will inflate dataview
-	 * 
 	 * @param offset default 0, a point from which deflate
 	 */
 	public inflate(offset: number = 0){
@@ -46,6 +54,10 @@ class ASDataView {
 		this.offset = 0; //restart position
 	}
 	
+	/**
+	 * get bytes
+	 * @param repeat number of bytes to retrieve
+	 */
 	public getBytes(repeat: number = 1): Uint8Array{
 		this.offset += repeat;
 		return new Uint8Array(this._data.buffer.slice(this.offset-repeat, repeat));
@@ -94,48 +106,80 @@ class ASDataView {
 		return result;
 	}
 	
+	/**
+	 * @param repeat number of Int8(s) to retrieve
+	 * @return array of 8-bit length Int(s)
+	 */	
 	public getInt8(repeat: number = 1): Array<number>{
 		return this._repeater(repeat, (offset: number) => {
 			return this._data.getInt8(offset);
 			}, 1);
 	}
-	
+
+	/**
+	 * @param repeat number of UInt8(s) to retrieve
+	 * @return array of 8-bit length UInt(s)
+	 */	
 	public getUint8(repeat: number = 1): Array<number>{
 		return this._repeater(repeat, (offset: number) => {
 			return this._data.getUint8(offset);
 			}, 1);
 	}
 	
+	/**
+	 * @param repeat number of Int16(s) to retrieve
+	 * @return array of 16-bit length Int(s)
+	 */	
 	public getInt16(repeat: number = 1): Array<number>{
 		return this._repeater(repeat, (offset: number) => {
 			return this._data.getInt16(offset, true);
 			}, 2);
 	}
 	
+	/**
+	 * @param repeat number of UInt16(s) to retrieve
+	 * @return array of 16-bit length UInt(s)
+	 */	
 	public getUint16(repeat: number = 1): Array<number>{
 		return this._repeater(repeat, (offset: number) => {
 			return this._data.getUint16(offset, true);
 			}, 2);
 	}
 	
+	/**
+	 * @param repeat number of Int32(s) to retrieve
+	 * @return array of 32-bit length Int(s)
+	 */	
 	public getInt32(repeat: number = 1): Array<number>{
 		return this._repeater(repeat, (offset: number) => {
 			return this._data.getInt32(offset, true);
 			}, 4);
 	}
 	
+	/**
+	 * @param repeat number of UInt32(s) to retrieve
+	 * @return array of 32-bit length UInt(s)
+	 */	
 	public getUint32(repeat: number = 1): Array<number>{
 		return this._repeater(repeat, (offset: number) => {
 			return this._data.getUint32(offset, true);
 			}, 4);
 	}
 	
+	/**
+	 * @param repeat number of float32(s) to retrieve
+	 * @return array of 32-bit length float(s)
+	 */	
 	public getFloat32(repeat: number = 1): Array<number>{
 		return this._repeater(repeat, (offset: number) => {
 			return this._data.getFloat32(offset, true);
 			}, 4);
 	}
 	
+	/**
+	 * @param repeat number of float64(s) to retrieve
+	 * @return array of 64-bit length float(s)
+	 */	
 	public getFloat64(repeat: number = 1): Array<number>{
 		return this._repeater(repeat, this._data.getFloat64, 8);
 	}
@@ -148,12 +192,8 @@ class ASDataView {
 	 */
 	private _repeater(repeat: number, callback: Function, size: number): Array<number>{
 		let result: Array<any> = new Array<any>();
-		//console.log("at start");
-		//console.log(callback);
 		let getfnc = callback.bind(this._data);			// bind function to _data object, otherwise we'll get an erro
 		for(let i: number = 0; i < repeat; i++){
-			//console.log(this.offset);
-			//console.log(getfnc);
 			result.push(getfnc(this.offset));
 			this.offset += size;
 		}
